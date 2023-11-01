891d3efb14178a6410b24136a1b7b2cc001a5ade
Marcelo Bracet <marcelo.bracet1@gmail.com>
2023-10-31 22:23:15 -0300

    feat: connecting to database

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,24 +1,67 @@
-import React from 'react'
-import Logo from '../components/logo/page'
-import Link from 'next/link';
+"use client";
 
-import './login.css'
+import React, { useState } from "react";
+import axios from "axios";
+
+import Logo from "../components/logo/page";
+import Link from "next/link";
+
+import "./login.css";
 
 const page: React.FC = () => {
-  return (
-    <div className='login-page-wrapper'>
-        <Logo />
-        <div className='login-form-wrapper'>
-          <form className='login-form' action="">
-            <h2>Login</h2>
-            <input placeholder='Email' className='login-form-input' type="email" />
-            <input placeholder='Senha' className='login-form-input' type="password" />
-            <input className='login-form-button login-form-input' type="submit" value="Entrar" />
-          </form>
-          <span>Não tem cadastro? <Link href="/register">Cadastre-se</Link></span>
-        </div>
-    </div>
-  )
-}
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
-export default page
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      let response = await axios.post("http://localhost:3001/login", {
+        email,
+        password,
+      });
+
+      // TODO: Levar para a página home quando implementada
+      alert("Login efetuado com sucesso");
+      setEmail("");
+      setPassword("");
+    } catch (err) {
+      alert("Login inválido");
+    }
+  };
+
+  return (
+    <div className="login-page-wrapper">
+      <Logo />
+      <div className="login-form-wrapper">
+        <form onSubmit={(e) => handleSubmit(e)} className="login-form">
+          <h2>Login</h2>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            className="login-form-input"
+            type="email"
+          />
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Senha"
+            className="login-form-input"
+            type="password"
+          />
+          <input
+            className="login-form-button login-form-input"
+            type="submit"
+            value="Entrar"
+          />
+        </form>
+        <span>
+          Não tem cadastro? <Link href="/register">Cadastre-se</Link>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default page;
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,27 +1,91 @@
-import React from 'react';
-import Logo from '../components/logo/page'
-import './register.css'
-import Link from 'next/link';
+"use client";
 
-// TELA DE PERFIL
+import React, { useState } from "react";
+import Logo from "../components/logo/page";
+import "./register.css";
+import Link from "next/link";
+
+import axios from "axios";
 
 const Register: React.FC = () => {
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassowrd] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+
+  const handleCadaster = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      let response = await axios.post("http://localhost:3001/create-user", {
+        name,
+        email,
+        password,
+        confirmPassword,
+      });
+
+      if (response.status !== 200) {
+        alert("Cadastro inválido");
+      }
+
+      alert("Usuário cadastrado com sucesso");
+      setName("");
+      setEmail("");
+      setPassowrd("");
+      setConfirmPassword("");
+
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <div className='register-page-wrapper'>
-        <Logo />
-        <div className='register-form-wrapper'>
-          <form className='register-form' action="">
-            <h2>Cadastre-se</h2>
-            <input placeholder='Nome completo' className='register-form-input' type="text" />
-            <input placeholder='Email' className='register-form-input' type="email" />
-            <input placeholder='Senha' className='register-form-input' type="password" />
-            <input placeholder='Confirmar senha' className='register-form-input' type="password" />
-            <input className='register-form-button register-form-input' type="submit" value="Cadastrar" />
-          </form>
-          <span>Já tem cadastro? <Link href="/login">Conecte-se</Link></span>
-        </div>
+    <div className="register-page-wrapper">
+      <Logo />
+      <div className="register-form-wrapper">
+        <form onSubmit={(e) => handleCadaster(e)} className="register-form">
+          <h2>Cadastre-se</h2>
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Nome completo"
+            className="register-form-input"
+            type="text"
+          />
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            className="register-form-input"
+            type="email"
+          />
+          <input
+            value={password}
+            onChange={(e) => setPassowrd(e.target.value)}
+            placeholder="Senha"
+            className="register-form-input"
+            type="password"
+          />
+          <input
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            placeholder="Confirmar senha"
+            className="register-form-input"
+            type="password"
+          />
+          <input
+            className="register-form-button register-form-input"
+            type="submit"
+            value="Cadastrar"
+          />
+        </form>
+        <span>
+          Já tem cadastro? <Link href="/login">Conecte-se</Link>
+        </span>
+      </div>
     </div>
   );
-}
+};
 
 export default Register;
